6a44d9eeacd77e6d37efa2b9cc1534ea8908963c
Go0osee <93539552+Go0osee@users.noreply.github.com>
2022-03-26 20:43:46 +0300

    first commit correction

--- app/src/main/java/com/example/retrofitjsonnews/domain/NewsRepository.kt
+++ app/src/main/java/com/example/retrofitjsonnews/domain/NewsRepository.kt
@@ -1,13 +1,8 @@
 package com.example.retrofitjsonnews.domain
 
-import com.example.retrofitjsonnews.data.NewsRepositoryImpl
-import com.example.retrofitjsonnews.data.RetrofitClient
 import com.example.retrofitjsonnews.data.model.NewsResponse
 
 interface NewsRepository {
     suspend fun getNews(searchWord: String): NewsResponse
 }
 
-fun getRepository(): NewsRepository = NewsRepositoryImpl(
-    api = RetrofitClient.getNewsApi()
-)
